e6431131cdbd584da59ad912164f977953685137
Alberto Cardellino <alberto.cardellino@iit.it>
2018-06-18 11:23:03 +0200

    Add StateMaster component for testing

---
+++ V3/TestV3_StateMaster/Debug/smartsoft/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../smartsoft/src/CompHandler.cc \
+../smartsoft/src/ParameterStateStruct.cc \
+../smartsoft/src/SmartStateChangeHandler.cc \
+../smartsoft/src/TestV3_StateMasterCore.cc 
+
+CC_DEPS += \
+./smartsoft/src/CompHandler.d \
+./smartsoft/src/ParameterStateStruct.d \
+./smartsoft/src/SmartStateChangeHandler.d \
+./smartsoft/src/TestV3_StateMasterCore.d 
+
+OBJS += \
+./smartsoft/src/CompHandler.o \
+./smartsoft/src/ParameterStateStruct.o \
+./smartsoft/src/SmartStateChangeHandler.o \
+./smartsoft/src/TestV3_StateMasterCore.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+smartsoft/src/%.o: ../smartsoft/src/%.cc
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
