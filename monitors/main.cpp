10af6881a5cfa5779e85fab3fcfd812111dba56e
mcolledanchise <michele.colledanchise@iit.it>
2019-02-27 15:43:21 +0100

    error handling in main

--- monitors/main.cpp
+++ monitors/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <yarp/os/Network.h>
 #include <monitor.h>
+#include <yarp/os/LogStream.h>
+
 using namespace std;
 using namespace yarp::os;
 
@@ -9,7 +11,16 @@ int main(int argc, char * argv[])
     Network yarp;
     Monitor module;
     ResourceFinder rf;
-    rf.setDefaultConfigFile("config.ini"); //file in scenario-config-repo
+    rf.configure(argc, argv);
+
+    if(!rf.setDefaultConfigFile("./config.ini")) //file in scenario-config-repo
+    {
+        yError() << "config.ini not found";
+        return 1;
+    }
+
+
+
     rf.setVerbose(true);
 
     cout << "Configuring and starting module. \n";
